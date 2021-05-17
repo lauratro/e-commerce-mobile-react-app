7ca3e65346db1998b37c34f69f019de8d09ba63b
Laura <65158950+lauratro@users.noreply.github.com>
2021-05-17 20:18:23 +0200

    single product card

--- src/components/Single-Product.js
+++ src/components/Single-Product.js
@@ -6,20 +6,64 @@ import {
   Link,
   useParams,
 } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  },
+  media: {
+    width: "50%",
+    margin: "0 auto",
+  },
+  fontBold: {
+    fontWeight: "bold",
+  },
+}));
 
 function SingleProduct(props) {
   let { id } = useParams();
   let [product, setProduct] = useState({});
+  const classes = useStyles();
   fetch(`https://fakestoreapi.com/products/${id}`)
     .then((res) => res.json())
     .then((json) => setProduct(json));
-  console.log(product);
+
   return (
     <div>
-      <p>{id}</p>
-      <p>{product.title}</p>
-      <p>{product.description}</p>
-      <img src={product.image} alt="product image" />
+      <div className={classes.root}>
+        <Grid container spacing={3} alignItems="center">
+          <Grid item xs={12}>
+            <Paper className={classes.paper} className={classes.fontBold}>
+              {product.title}
+            </Paper>
+          </Grid>
+          <Grid item xs={6}>
+            <img
+              className={classes.media}
+              src={product.image}
+              alt={product.title}
+            />
+          </Grid>
+          <Grid item xs={6}>
+            <Paper className={classes.paper}>Price: {product.price}$</Paper>
+
+            <Paper className={classes.paper}>
+              Category: {product.category}
+            </Paper>
+          </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>{product.description}</Paper>
+          </Grid>
+        </Grid>
+      </div>
     </div>
   );
 }
