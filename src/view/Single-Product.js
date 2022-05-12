ceff266ff34df50a9785893be70a89d41f73e275
lauratro <lauratronchin@hotmail.it>
2022-05-12 17:50:38 +0200

    Clean code

--- src/view/Single-Product.js
+++ src/view/Single-Product.js
@@ -13,7 +13,7 @@ import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
 
 import { useMediaQuery } from "react-responsive";
 
-import { TextField, Button } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -34,6 +34,9 @@ const useStyles = makeStyles((theme) => ({
     fontWeight: "bold",
     margin: 10,
     paddingBottom: 10,
+    padding: theme.spacing(2),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
   },
 }));
 
@@ -48,23 +51,18 @@ function SingleProduct(props) {
   const db = myfirebase.firestore();
   const { user, setUser } = useContext(AuthContext);
   const {
-    docProduct,
-    setDocProduct,
-    idProductArray,
-    setIdProductArray,
     objectQuantity,
     setObjectQuantity,
     allIdProductArray,
-    setAllIdProductArray,
   } = useContext(VariablesContext);
-  // console.log("spobj", objectQuantity);
+
   let { id } = useParams();
   let [product, setProduct] = useState();
-  let [buttonBuy, setButtonBuy] = useState(false);
+
   let [buttonFav, setButtonFav] = useState(false);
-  const [shoppingCart, setShoppingCart] = useState({});
+
   const classes = useStyles();
-  // console.log("array", docProduct);
+
   useEffect(() => {
     const fetchData = () => {
       fetch(`https://fakestoreapi.com/products/${id}`)
@@ -73,29 +71,6 @@ function SingleProduct(props) {
     };
     fetchData();
 
-    /*   if (user) {
-      db.collection("shopping")
-        .where("uid", "==", user.uid)
-        .get()
-        .then((querySnapshot) => {
-          let arrayId = [];
-          querySnapshot.forEach((doc) => {
-            // console.log(doc.id, " => ", doc.data());
-
-            let docData = doc.data();
-
-            //Product id
-            let idProd = docData.product.id;
-
-            arrayId.push(idProd);
-          });
-          setIdProductArray(arrayId);
-          // console.log("doc", idProductArray);
-        })
-        .catch((error) => {
-          console.log("Error getting documents: ", error);
-        });
-    } */
     //Favorites
     if (user) {
       db.collection("favorites")
@@ -103,7 +78,7 @@ function SingleProduct(props) {
         .get()
         .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
-            // console.log(doc.id, " spfav=> ", doc.data());
+       
 
             let docData = doc.data();
 
@@ -121,8 +96,6 @@ function SingleProduct(props) {
         });
     }
   }, []);
-  //console.log("afterFetch", idProductArray);
-  //console.log("afterFetch", buttonBuy);
 
   const onClickBtnFav = () => {
     setButtonFav(true);
@@ -137,7 +110,7 @@ function SingleProduct(props) {
           uid: user.uid,
         })
         .then((doc) => {
-          // console.log("newfav", doc.id);
+         
           db.collection("favorites").doc(doc.id).update({
             docId: doc.id,
           });
@@ -154,25 +127,24 @@ function SingleProduct(props) {
   };
   // -------------------------------------End Favorites-------------------------------
 
-  //console.log("kv", objectKeyValue);
 
   useEffect(() => {
-    // console.log("externa", allIdProductArray);
+  
     console.log("checking shoppingcart");
     db.collection("shopping")
       .get()
       .then((querySnapshot) => {
         let arrayIdGen = [];
         querySnapshot.forEach((doc) => {
-          // console.log(doc.id, " => ", doc.data());
+     
           let docDatagen = doc.data();
-          // console.log(docDatagen);
+   
           let idDocGen = docDatagen.product.id;
-          // console.log("idgen", idDocGen);
+       
 
           arrayIdGen.push(idDocGen);
         });
-        /* setAllIdProductArray(arrayIdGen); */
+       
         console.log("idquantity", allIdProductArray);
         // Define how many time a object is repeated
         var objectKeyValue = arrayIdGen.reduce(function (acc, curr) {
@@ -190,7 +162,7 @@ function SingleProduct(props) {
 
   //Add product to Shopping Collection Firebase
   const handleBuy = () => {
-    //   console.log(user);
+
     if (user) {
       db.collection("shopping")
         .add({
@@ -198,16 +170,14 @@ function SingleProduct(props) {
           uid: user.uid,
         })
         .then((doc) => {
-          //    console.log("new", doc.id);
+        
           db.collection("shopping")
             .doc(doc.id)
             .update({
               docId: doc.id,
             })
             .then(() => {
-              // console.log(objectQuantity.hasOwnProperty(product.id));
-              // console.log("then", objectQuantity);
-              // console.log("idthen", product.id);
+             
 
               if (objectQuantity) {
                 if (objectQuantity.hasOwnProperty(product.id)) {
@@ -223,21 +193,7 @@ function SingleProduct(props) {
                 }
               }
 
-              /* var objectKeyValue = allIdProductArray.reduce(function (
-                acc,
-                curr
-              ) {
-                if (typeof acc[curr] == "undefined") {
-                  acc[curr] = 1;
-                } else {
-                  acc[curr] += 1;
-                }
-
-                return acc;
-              },
-              {});
-              console.log("objectKeyValue", objectKeyValue);
-              setObjectQuantity(objectKeyValue); */
+         
             });
         })
         .catch((error) => {
@@ -245,9 +201,9 @@ function SingleProduct(props) {
         });
     }
   };
-  // console.log("quantity", objectQuantity);
+
   return (
-    // <Page title={product.title}>
+
     <div>
       {product && (
         <div className={classes.root}>
@@ -263,7 +219,7 @@ function SingleProduct(props) {
           >
             <Grid item xs={12} sm={6}>
               <Paper
-                className={classes.paper}
+             
                 className={classes.fontBoldTitle}
               >
                 {product.title}
