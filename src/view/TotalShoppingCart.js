0e4c54c41e49083dc6ae08872eb530029864ca88
lauratro <lauratronchin@hotmail.it>
2022-05-12 17:40:26 +0200

    Clean code

--- src/view/TotalShoppingCart.js
+++ src/view/TotalShoppingCart.js
@@ -1,19 +1,19 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import Paper from "@material-ui/core/Paper";
 import Grid from "@material-ui/core/Grid";
 import { VariablesContext } from "../context/ContextStorage";
 import CheckboxMentor from "../components/CheckboxMentor";
 
 export default function TotalShoppingCart() {
-  const { docProduct, setDocProduct, priceCart, setPriceCart } =
+  const { docProduct, priceCart, setPriceCart } =
     useContext(VariablesContext);
-  //const [threeProdDiscountText, setThreeProdDiscountText] = useState(false);
+
   let amount = 0;
   console.log("totaldoc", docProduct);
 
   if (priceCart.length > 0) {
     amount = priceCart.reduce((a, c) => a + c, 0);
-    console.log("amount", amount);
+  
     let rounded = amount.toFixed(2);
     let roundedPrice = Number(rounded);
     setPriceCart(roundedPrice);
@@ -21,10 +21,6 @@ export default function TotalShoppingCart() {
     amount = 0;
   }
 
-  /*  if (docProduct.length >= 3) {
-    let priceThreeProd = priceCart * 0.9;
-    setPriceCart(priceThreeProd);
-  } */
   return (
     <div display="flex">
       <Grid
